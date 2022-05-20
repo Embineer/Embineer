d04d36aea379a8adaae60b6e778c4c7fd1eb25de
Lilmarziq <ammar-haziq.bin-mohd-halim@stud.hshl.de>
2022-05-21 01:10:34 +0200

    Car Header

---
+++ Car.h
@@ -0,0 +1,18 @@
+// Car.h
+#ifndef Car_h
+#define Car_h
+
+#include <Arduino.h>
+
+class Car {
+  private:
+
+  public:
+    int Duration;
+    int Allowed;
+    int Time_Arrival;
+    int Leave;
+    
+};
+
+#endif
