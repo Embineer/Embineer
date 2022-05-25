0fa8662645d99a2f2101120e02bf0cfa4cd06f38
Adib6637 <sheikh-muhammad-adib.bin-sh-abu-bakar@stud.hshl.de>
2022-05-25 22:09:16 +0200

    Update resource_manager.ino

--- Code/resource_manager/resource_manager.ino
+++ Code/resource_manager/resource_manager.ino
@@ -1,9 +1,9 @@
-
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h>
 #include <queue.h>
 
-#define QUEUE_SIZE 10
+#define QUEUE_SIZE 100
+#define STACK_SIZE 128
 #define BUTTON 13
 
 // car class to create dummy car
@@ -39,22 +39,21 @@ car::car(short id, short duration,short time_Arrival,short leave,short s1,short 
 
 // car class to create dummy car
 car* car1 = new car( 1,2,21,23,11,0,6,0,0,0);// dummy car
-car* car2 = new car( 2,2,21,23,0,0,8,0,7,10);// dummy car
-// car* car3 = new car( 3,2,21,23,11,0,6,0,0,0);// dummy car
-/*
-car* car4 = new car( 4,2,21,23,0,0,8,0,7,10);// dummy car
-car* car5 = new car( 5,2,21,23,11,0,6,0,0,0);// dummy car
-car* car6 = new car( 6,2,21,23,0,0,8,0,7,10);// dummy car
-car* car7 = new car( 7,2,21,23,0,0,8,0,7,10);// dummy car
-car* car8 = new car( 8,2,21,23,11,0,6,0,0,0);// dummy car
-car* car9 = new car( 9,2,21,23,0,0,8,0,7,10);// dummy car
-car* car10 = new car( 10,2,21,23,0,0,8,0,7,10);// dummy car
-car* car11 = new car( 11,2,21,23,11,0,6,0,0,0);// dummy car
-car* car12 = new car( 12,2,21,23,0,0,8,0,7,10);// dummy car
-car* car13 = new car( 13,2,21,23,0,0,8,0,7,10);// dummy car
-car* car14 = new car( 14,2,21,23,11,0,6,0,0,0);// dummy car
-car* car15 = new car( 15,2,21,23,0,0,8,0,7,10);// dummy car
-*/
+car* car2 = new car( 2,3,21,23,0,0,8,0,7,10);// dummy car
+car* car3 = new car( 1,2,21,23,11,0,6,0,0,0);// dummy car
+car* car4 = new car( 2,3,21,23,0,0,8,0,7,10);// dummy car
+car* car5 = new car( 1,2,21,23,11,0,6,0,0,0);// dummy car
+car* car6 = new car( 2,3,21,23,0,0,8,0,7,10);// dummy car
+car* car7 = new car( 1,2,21,23,11,0,6,0,0,0);// dummy car
+car* car8 = new car( 2,3,21,23,0,0,8,0,7,10);// dummy car
+car* car9 = new car( 1,2,21,23,11,0,6,0,0,0);// dummy car
+car* car10 = new car( 2,3,21,23,0,0,8,0,7,10);// dummy car
+car* car11 = new car( 1,2,21,23,11,0,6,0,0,0);// dummy car
+car* car12 = new car( 2,3,21,23,0,0,8,0,7,10);// dummy car
+car* car13 = new car( 1,2,21,23,11,0,6,0,0,0);// dummy car
+car* car14 = new car( 2,3,21,23,0,0,8,0,7,10);// dummy car
+car* car15 = new car( 1,2,21,23,11,0,6,0,0,0);// dummy car
+car* car16 = new car( 2,3,21,23,0,0,8,0,7,10);// dummy car
 
 // car queue
 QueueHandle_t car_queue;
@@ -83,13 +82,11 @@ void reset(car*chosen_car);
 void lock(short resource);
 void unlock(short resource);
 
-// guard for executing dummy car
-int filled = 0;
 
 void setup() {
 
   Serial.begin(1200);
-
+  
   // guard for queue and each resources
   mutexQueue = xSemaphoreCreateMutex();
   mutex1 = xSemaphoreCreateMutex();
@@ -105,28 +102,42 @@ void setup() {
   mutex11 = xSemaphoreCreateMutex();
   mutex12 = xSemaphoreCreateMutex();
 
+  // initialize car queue  
+  car_queue = xQueueCreate(QUEUE_SIZE, sizeof(car*));  
+
   // tasks
-  xTaskCreate(resource_manager, "Task1", 128,1,1 , NULL);
-  xTaskCreate(resource_manager, "Task2", 128,1,1 , NULL);
-  //xTaskCreate(resource_manager, "Task3", 1280,1,1 , NULL);
-  // xTaskCreate(resource_manager, "Task4", 1280,1,1 , NULL);
+  xTaskCreate(resource_manager, "Task1", STACK_SIZE,1,1 , 0);
+  xTaskCreate(resource_manager, "Task2", STACK_SIZE,1,1 , 0);
+  xTaskCreate(resource_manager, "Task3", STACK_SIZE,1,1 , 0);
+  xTaskCreate(resource_manager, "Task4", STACK_SIZE,1,1 , 0);
 
-  // initialize car queue
-  car_queue = xQueueCreate(10, sizeof(car*));
+  // put dummy car in queue
+  xQueueSendToBack(car_queue, (void*)&car2, 1);
+  xQueueSendToBack(car_queue, (void*)&car1, 1);
+  xQueueSendToBack(car_queue, (void*)&car3, 1);
+  xQueueSendToBack(car_queue, (void*)&car4, 1);
+  xQueueSendToBack(car_queue, (void*)&car5, 1);
+  xQueueSendToBack(car_queue, (void*)&car6, 1);
+  xQueueSendToBack(car_queue, (void*)&car7, 1);
+  xQueueSendToBack(car_queue, (void*)&car8, 1);
+  xQueueSendToBack(car_queue, (void*)&car9, 1);
+  xQueueSendToBack(car_queue, (void*)&car10, 1);
+  xQueueSendToBack(car_queue, (void*)&car11, 1);
+  xQueueSendToBack(car_queue, (void*)&car12, 1);
+  xQueueSendToBack(car_queue, (void*)&car13, 1);
+  xQueueSendToBack(car_queue, (void*)&car14, 1);
+  xQueueSendToBack(car_queue, (void*)&car15, 1);
+  xQueueSendToBack(car_queue, (void*)&car16, 1);
 
-  // button for simulating car request
-  pinMode(13, INPUT);
-  delay(10);
+  // guard to add more car into queue
+  pinMode(BUTTON,INPUT);
 }
 
 void loop() {
-  // put dummy car in queue
-  
-  while(filled==0){
+  if(digitalRead(BUTTON) == 1){
     xQueueSendToBack(car_queue, (void*)&car2, 1);
     xQueueSendToBack(car_queue, (void*)&car1, 1);
-    // xQueueSendToBack(car_queue, (void*)&car3, 1);
-    /*
+    xQueueSendToBack(car_queue, (void*)&car3, 1);
     xQueueSendToBack(car_queue, (void*)&car4, 1);
     xQueueSendToBack(car_queue, (void*)&car5, 1);
     xQueueSendToBack(car_queue, (void*)&car6, 1);
@@ -139,47 +150,41 @@ void loop() {
     xQueueSendToBack(car_queue, (void*)&car13, 1);
     xQueueSendToBack(car_queue, (void*)&car14, 1);
     xQueueSendToBack(car_queue, (void*)&car15, 1);
-    */
-    filled = 1;
+    xQueueSendToBack(car_queue, (void*)&car16, 1);
   }
-  if(digitalRead(13)== 1){
-    filled==0;
-  }
-  delay(100);
 }
 
+
 void resource_manager(void *pvParameters){
-  (void) pvParameters;
   Serial.println("new"); 
   car car_handler = car( 0,0,0,0,0,0,0,0,0,0);
   for (;;)
   {
     listesning();// check for requesting car on queue
-    Serial.println("pass listening");
     choose(&car_handler);// take car to handle
-    Serial.println("pass choose");
+    
+  Serial.println(car_handler.Duration); 
     checking(&car_handler);// lock requested resources
     allocate(&car_handler);// give permission to the car to use the resources
     reset(&car_handler);// unlock requested resources
-    delay(10);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
 
 // check for requesting car on queue
 void listesning(){
-  int car_exist = 0;
-  while(car_exist == 0){
-    if(car_queue != 0){
-      car_exist = 1;
-    }    
-  }
+  for(; uxQueueSpacesAvailable(car_queue) == QUEUE_SIZE;){
+  } // Check first element 
 }
 
 // take a car from queue to handle the car
 void choose(car *chosen_car){
+  car* choosing_car;
   while(xSemaphoreTake(mutexQueue, 10)!= pdTRUE){};
-  xQueueReceive( car_queue, chosen_car, ( TickType_t ) 10 );
+  xQueueReceive( car_queue,&(choosing_car), ( TickType_t ) 10 );
   while(xSemaphoreGive(mutexQueue)!= pdTRUE){}; 
+  *chosen_car=*choosing_car;
+
 }
 
 // lock requested resources
@@ -203,7 +208,7 @@ void checking(car *chosen_car){
       lock(chosen_car->S6);
     }
 }
-    
+  
 void lock(short resource){
   switch (resource){
     case 1 :  while(xSemaphoreTake(mutex1, 10)!= pdTRUE);Serial.println("Source 1 locked"); break;
@@ -222,7 +227,7 @@ void lock(short resource){
 }
 // give permission to the car to use the resources
 void allocate(car*choosen_car){
-  delay(((choosen_car->Duration)*30)/ portTICK_PERIOD_MS);
+  vTaskDelay(((choosen_car->Duration)*30)/ portTICK_PERIOD_MS);
 }
 
 // unlock requested resources
